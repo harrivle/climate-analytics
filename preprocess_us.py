c6517d8b9e3e5eaef9473db4099517bf50e3b725
Harrison Le <harrivle@gmail.com>
2021-04-24 17:15:11 -0700

    infra stuff; remove dupes from dataset

--- preprocess_us.py
+++ preprocess_us.py
@@ -1,25 +1,11 @@
-import re
-
 import numpy as np
 import pandas as pd
-import datetime as dt
-import gc
-import random
-import sklearn
-from sklearn.model_selection import cross_val_score, GridSearchCV, cross_validate, train_test_split
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.svm import SVC
-from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler, normalize
-from sklearn.decomposition import PCA
-from sklearn.impute import SimpleImputer
 
 
 def main():
     dis_path = './data/us_disaster_declarations.csv'
     temp_path = './data/GlobalLandTemperaturesByState.csv'
+    # temp_path = './data/city_temperature.csv'
     states_path = './data/states.csv'
 
     # load list of states
@@ -34,15 +20,12 @@ def main():
     # print(states)
 
     # filter disaster dataset
-    dis_data = pd.read_csv(dis_path)[['state', 'declaration_date', 'incident_type', 'declaration_title']].rename(
-        {'declaration_date': 'date'}, axis=1)
-    dis_data['date'] = dis_data['date'].astype('datetime64[ns]').dt.strftime('%m-%Y')
-    dis_data = dis_data.drop_duplicates(subset=['incident_type', 'declaration_title', 'date', 'state'], keep='first')
-    # dis_data = dis_data.groupby(['state', 'date']).count()
-    dis_data['disaster_type'] = dis_data['incident_type']
+    dis_data = pd.read_csv(dis_path)[['state', 'declaration_date', 'incident_type']].rename({'declaration_date': 'date'}, axis=1)
+    dis_data['date'] = dis_data['date'].astype('datetime64[ns]').dt.strftime('%Y-%m')
+    dis_data = dis_data.groupby(['state', 'date']).count()
     dis_data = dis_data.rename({'incident_type': 'disaster_occurrence'}, axis=1)
     dis_data['disaster_occurrence'] = np.ones(dis_data['disaster_occurrence'].shape)
-    # dis_data = dis_data.reset_index()
+    dis_data.reset_index(inplace=True)
 
     # print(dis_data)
 
@@ -51,14 +34,19 @@ def main():
     # filter temperature dataset
     temp_data = pd.read_csv(temp_path)
     temp_data = temp_data[temp_data['Country'] == 'United States'].dropna()  # filter by United States, remove NaNs
-    temp_data['date'] = temp_data['dt'].astype('datetime64[ns]').dt.strftime(
-        '%m-%Y')  # convert string to date, then convert to year
-    temp_data['state'] = temp_data['State'].apply(
-        lambda x: states[x] if x in states else None)  # preprocess state strings
-    temp_data = temp_data.dropna()
+    temp_data['date'] = temp_data['dt'].astype('datetime64[ns]').dt.strftime('%Y-%m')  # convert string to date, then convert to year
+    temp_data['state'] = temp_data['State'].apply(lambda x: states[x] if x in states else None)  # preprocess state strings
+    temp_data = temp_data.dropna().rename({'AverageTemperature': 'ave_temp', 'AverageTemperatureUncertainty': 'ave_temp_uncertainty'}, axis=1)
     temp_data = temp_data.groupby(['date', 'state'])  # group by year then state
-    temp_data = temp_data[
-        ['AverageTemperature', 'AverageTemperatureUncertainty']].mean().reset_index()  # take average over groups
+    temp_data = temp_data[['ave_temp', 'ave_temp_uncertainty']].mean().reset_index()  # take average over groups
+
+    # temp_data = pd.read_csv(temp_path)
+    # temp_data = temp_data[temp_data['Country'] == 'US'].dropna()  # filter by United States, remove NaNs
+    # temp_data['date'] = temp_data.apply(lambda x: '{0}-{1}-{2}'.format(x['Year'], x['Month'], x['Day']), axis=1)  # convert string to date, then convert to year
+    # temp_data['state'] = temp_data['State'].apply(lambda x: states[x] if x in states else None)  # preprocess state strings
+    # temp_data = temp_data.dropna().rename({'AvgTemperature': 'ave_temp'}, axis=1)
+    # temp_data = temp_data.groupby(['date', 'state'])  # group by year then state
+    # temp_data = temp_data[['ave_temp']].mean().reset_index()  # take average over groups
 
     # print(temp_data)
 
@@ -66,25 +54,15 @@ def main():
 
     # join on `Year` and `State`
     df = pd.merge(temp_data, dis_data, on=['date', 'state'], how='left').set_index(['date', 'state'], drop=True)
-    df.rename({'AverageTemperature': 'ave_temp', 'AverageTemperatureUncertainty': 'ave_temp_uncertainty'}, axis=1,
-              inplace=True)
     df = df.fillna(0).reset_index()
     df['month'] = df['date'].astype('datetime64[ns]').dt.strftime('%m')
 
-    df.to_csv('./data/test_disasters_temp_state_month.csv', index=False)
-
-    df['y_data'] = df['disaster_occurrence']
-    df = df.drop(['disaster_occurrence'], axis=1)
     print(df)
 
-    x_data = df.iloc[:, 0:-1]
-    y_data = df.iloc[:, -1]
-    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=.7, random_state=614, shuffle=True)
+    df.to_csv('./data/test_disasters_temp_state_month.csv', index=False)
 
-    print(x_data)
-    print(y_data)
+    print(df[df['disaster_occurrence'] == 1.0].shape)
 
 
 if __name__ == '__main__':
     main()
-
